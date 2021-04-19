75d30ad871f50c95a98e4c9fa4dc7f4277bc73cd
Yam86rus <yam86rus@gmail.com>
2021-04-19 10:12:11 +0500

    add edit to Product List

--- src/main/java/com/example/learningonetomany/product/ProductController.java
+++ src/main/java/com/example/learningonetomany/product/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -38,6 +39,16 @@ public class ProductController {
         List<Product> listProducts = productRepo.findAll();
         model.addAttribute("listProducts",listProducts);
         return "products";
+    }
+
+    @GetMapping("products/edit/{id}")
+    public String showEditProductForm(@PathVariable("id") Integer id, Model model) {
+        Product product = productRepo.findById(id).get();
+        model.addAttribute("product",product);
+        List<Category> listCategories = categoryRepo.findAll();
+        model.addAttribute("listCategories", listCategories);
+
+        return "product_form";
     }
 
 }
